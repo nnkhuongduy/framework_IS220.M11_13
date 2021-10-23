9e30dc6e3afafede69329d4d83e58d998d191611
KhuongDuy <60511087+nnkhuongduy@users.noreply.github.com>
2021-10-23 21:08:39 +0700

    implementing amazon s3 for uploading (#4)

---
+++ Options/AmazonS3Options.cs
@@ -0,0 +1,15 @@
+using _99phantram.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace _99phantram.Options
+{
+  public class AmazonS3Options: IAmazonS3Options {
+    public string AvatarsBucket {get; set;}
+
+    public AmazonS3Options(IConfiguration config) {
+      var appSettings = config.GetSection(nameof(AmazonS3Options));
+
+      AvatarsBucket = appSettings["AvatarsBucket"];
+    }
+  }
+}
