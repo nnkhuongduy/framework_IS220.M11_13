12a650a98f63bbed571698cc3785dfb8d7d1856f
Duy Nguyen <84265774+RogueLeon@users.noreply.github.com>
2021-11-29 20:11:14 +0700

    Create Supply.cs

---
+++ Models/Supply.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using FluentValidation;
+
+using _99phantram.Entities;
+
+namespace _99phantram.Models
+{
+  public class ClientPostSupplySpec
+  {
+    public string Id { get; set; }
+    public string Value { get; set; }
+  }
+
+  public class ClientPostSupply
+  {
+    public string Name { get; set; }
+    public long Price { get; set; }
+    public string Description { get; set; }
+    public List<ClientPostSupplySpec> Specs { get; set; }
+    public List<string> Images { get; set; }
+    public string Thumbnail { get; set; }
+    public List<string> Categories { get; set; }
+    public List<string> Locations { get; set; }
+    public string Address { get; set; }
+  }
+
+  public class ClientPostSupplySpecValidator : AbstractValidator<ClientPostSupplySpec>
+  {
+    public ClientPostSupplySpecValidator()
+    {
+      RuleFor(_ => _.Id).NotEmpty();
+      RuleFor(_ => _.Value).NotEmpty();
+    }
+  }
+
+  public class ClientPostSupplyValidator : AbstractValidator<ClientPostSupply>
+  {
+    public ClientPostSupplyValidator()
+    {
+      RuleFor(_ => _.Name).NotEmpty();
+      RuleFor(_ => _.Price).NotEmpty().GreaterThan(0);
+      RuleFor(_ => _.Description).NotEmpty().Length(0, 500);
+      RuleFor(_ => _.Specs).NotEmpty().Must(_ => _.Count != 0);
+      RuleFor(_ => _.Images).NotEmpty().Must(_ => _.Count != 0);
+      RuleFor(_ => _.Thumbnail).NotEmpty();
+      RuleFor(_ => _.Categories).NotEmpty().Must(_ => _.Count == 2);
+      RuleFor(_ => _.Locations).NotEmpty().Must(_ => _.Count == 3);
+      RuleFor(_ => _.Address).NotEmpty();
+    }
+  }
+
+  public class SupplyQueryFilter
+  {
+    public int Page { get; set; }
+  }
+}
