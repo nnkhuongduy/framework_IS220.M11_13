33f6ac632ad2424348a45014718eae03e901c9c9
Duy Nguyen <84265774+RogueLeon@users.noreply.github.com>
2021-11-29 20:03:44 +0700

    Update Supply.cs

--- Entities/Supply.cs
+++ Entities/Supply.cs
@@ -1,41 +1,50 @@
 using System;
-using MongoDB.Bson;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Entities;
 
 namespace _99phantram.Entities
 {
   public enum SupplyStatus
   {
-    CREATED,
+    WAITING,
+    DECLINED,
     ACTIVE,
     SOLD,
-    REVIEWING,
     ARCHIVED
   }
 
   [Collection("supplies")]
   public class Supply : Entity, ICreatedOn, IModifiedOn
   {
+    [BsonIgnore]
+    public User Owner { get; set; }
     [Field("owner")]
-    public ObjectId Owner { get; set; }
+    public One<User> OwnerRef { get; set; }
     [Field("name")]
     public string Name { get; set; }
     [Field("price")]
     public long Price { get; set; }
     [Field("description")]
     public string Description { get; set; }
-    [Field("service")]
-    public Service[] Services { get; set; }
+    [Field("services")]
+    public List<Service> Services { get; set; }
     [Field("specs")]
-    public Spec[] Specs { get; set; }
+    public List<Spec> Specs { get; set; }
     [Field("images")]
-    public string[] Images { get; set; }
+    public List<string> Images { get; set; }
+    [Field("thumbnail")]
+    public string Thumbnail { get; set; }
     [Field("categories")]
-    public Category[] Categories { get; set; }
+    public List<Category> Categories { get; set; }
     [Field("locations")]
-    public Location[] Locations { get; set; }
+    public List<Location> Locations { get; set; }
+    [Field("address")]
+    public string Address { get; set; }
+    [Field("reason")]
+    public string Reason { get; set; }
     [Field("product_status")]
-    public ProductStatus Product_status { get; set; }
+    public ProductStatus ProductStatus { get; set; }
     [Field("status")]
     public SupplyStatus Status { get; set; }
     [Field("created_on")]
