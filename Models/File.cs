625683b39dede0571434ae06e00ab1d26c9ddf7a
thinhloi <19520691@gm.uit.edu.vn>
2021-11-29 19:44:53 +0700

    thêm chức năng supply

--- Models/File.cs
+++ Models/File.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace _99phantram.Models
 {
   public class GetFileResponse
   {
     public string Url { get; set; }
+  }
+
+  public class GetFilesResponse
+  {
+    public List<string> Urls { get; set; }
+
+    public GetFilesResponse()
+    {
+      Urls = new List<string>();
+    }
   }
 }
